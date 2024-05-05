d7d18dd4f6afd970cb3e9feb1e0c7f849cd6c7b2
DiegoElian02 <elianrdz2002@gmail.com>
2024-05-05 07:18:07 -0600

    os

--- interface/interface.py
+++ interface/interface.py
@@ -7,6 +7,8 @@ import pydeck as pdk
 import pickle
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderServiceError, GeocoderTimedOut
+import os
+print(os.path)
 
 with open('models/preprocessor.pkl', 'rb') as f:
     preprocessor = pickle.load(f)
